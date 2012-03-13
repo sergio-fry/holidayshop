0c0c82de30af5c4e28faf9f961ee694336032b91
Sergei O. Udalov <sergei.udalov@gmail.com>
2012-03-13 10:53:17 +0400

    rails + rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibbleApi::Application.config.secret_token = '42324ca0d9511674f3ae44f8614ba9c40e103b056e2d0114bc375b8d218390938a543a339d23564d80a0338cbe960399eca530b359c687ee78c23fe484b84ed6'
