2e6bd238f4a4edef3954ea72efbf36a6470aa4e0
Sergei O. Udalov <sergei.udalov@gmail.com>
2012-11-10 20:34:16 +0400

    active admin assets precompile, fallback on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ HolidayShop::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,7 +47,7 @@ HolidayShop::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( api.js )
+  config.assets.precompile += %w[active_admin.css active_admin.js]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
