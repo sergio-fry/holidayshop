1f787cfd37644ad9f340491008703e241f8e97b5
Sergei O. Udalov <sergei.udalov@gmail.com>
2013-04-24 00:19:32 +0400

    bucket created

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,6 @@ HolidayShop::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  # for Amazon S3
+  # for Amazon S3 
   config.assets.enabled = true
 end
