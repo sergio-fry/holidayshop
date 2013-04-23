e3ead71aac29ddffb2cd1d1f9eb1d9618d8c6c24
Sergei O. Udalov <sergei.udalov@gmail.com>
2013-04-24 00:05:24 +0400

    AWS S3 assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ HolidayShop::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
@@ -65,4 +66,6 @@ HolidayShop::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # for Amazon S3
+  config.assets.enabled = true
 end
