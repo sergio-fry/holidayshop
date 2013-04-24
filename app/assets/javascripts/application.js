73d4dc02458b907a99ecaa270d2f32be1c4fa885
Sergei O. Udalov <sergei.udalov@gmail.com>
2013-04-24 11:04:38 +0400

    Yandex metrika

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require turbolinks
 //= require jquery
+//= require jquery.turbolinks
+//= require turbolinks
 //= require jquery_ujs
 //= require bootstrap/js/bootstrap
