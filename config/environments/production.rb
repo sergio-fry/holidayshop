868dd067198cba4d6f40f617503696c094324c5e
Sergei O. Udalov <sergei.udalov@gmail.com>
2013-04-24 11:44:01 +0400

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,14 @@ HolidayShop::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   # for Amazon S3 
   config.assets.enabled = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
