770b909f823859df2d4739e7599c3cb0fa7d0d51
Sergei O. Udalov <sergei.udalov@gmail.com>
2013-04-24 10:43:54 +0400

    redirect to www.prazdnik-magazin.ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_www, :check_heroku_app if Rails.env.production?
+
   protect_from_forgery
+
+  private
+
+  def check_heroku_app
+    redirect_to request.protocol + "www.prazdnik-magazin.ru" + request.path if request.host.match(/heroku/)
+  end
+
+  def check_www
+    redirect_to request.protocol + "www." + request.host_with_port + request.path if !/^www/.match(request.host)
+  end
 end
